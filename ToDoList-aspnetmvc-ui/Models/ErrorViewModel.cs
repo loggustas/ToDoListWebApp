69451266c3aa66666eef54890cd44047321010fe
Gustas <loggustas@gmail.com>
2022-11-21 13:17:02 +0200

    Changed to net core 3.1.

--- ToDoList-aspnetmvc-ui/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ToDoList_aspnetmvc_ui.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
