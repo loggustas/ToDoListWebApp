4aeb3ddb363424c71d597a0ec8402e2e53395f4c
Gustas <loggustas@gmail.com>
2022-12-17 16:40:13 +0200

    User controller.

---
+++ ToDoList-netaspmvc/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList_DomainModel.ViewModels;
+
+namespace ToDoList_netaspmvc.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            return Ok();
+        }
+    }
+}
