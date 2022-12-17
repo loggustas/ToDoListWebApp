22ede4a7fb0dfc03fa0d64dcc7a030c2a21c3bd4
Gustas <loggustas@gmail.com>
2022-12-17 21:26:30 +0200

    Added methods for Registering the user.

--- ToDoList-netaspmvc/Models/Repo/IUserRepository.cs
+++ ToDoList-netaspmvc/Models/Repo/IUserRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace ToDoList_netaspmvc.Models.Repo
+﻿using ToDoList_DomainModel.ViewModels;
+
+namespace ToDoList_netaspmvc.Models.Repo
 {
     public interface IUserRepository
     {
+        public bool RegisterUser(RegisterViewModel registerViewModel);
+
+        public bool UsernameExists(string username);
     }
 }
--- ToDoList-netaspmvc/Models/Repo/UserRepository.cs
+++ ToDoList-netaspmvc/Models/Repo/UserRepository.cs
@@ -1,6 +1,43 @@
-﻿namespace ToDoList_netaspmvc.Models.Repo
+﻿using System.Linq;
+using ToDoList_DomainModel.Models;
+using ToDoList_DomainModel.ViewModels;
+using ToDoList_netaspmvc.Infrastructure;
+
+namespace ToDoList_netaspmvc.Models.Repo
 {
     public class UserRepository : IUserRepository
     {
+        private readonly ToDoContext _context;
+
+        public UserRepository(ToDoContext context)
+        {
+            _context = context;
+        }
+        public bool RegisterUser(RegisterViewModel registerViewModel)
+        {
+            User user = new User(registerViewModel);
+
+            _context.Users.Add(user);
+            int result = _context.SaveChanges();
+
+            if (result == 1)
+            {
+                return true;
+            }
+
+            return true;
+        }
+
+        public bool UsernameExists(string username)
+        {
+            User user = _context.Users.FirstOrDefault(x => x.Username == username);
+
+            if (user == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
