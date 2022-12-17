7f7b8ae74616d48d56ae21b293aa2258abc879ab
Gustas <loggustas@gmail.com>
2022-12-17 16:36:58 +0200

    Added register view model.

---
+++ ToDoList_DomainModel/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ToDoList_DomainModel.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [StringLength(256, MinimumLength = 2)]
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [Display(Name = "Email Adress")]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
